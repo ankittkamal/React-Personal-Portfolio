ded0176f4a33eb6a82e586dfaaaa540977a1cfd7
ankittkamal <officiallyankit999@gmail.com>
2023-10-17 13:23:06 +0530

    Added Sidebar menuItem

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div className="w-full xl:w-[1600px] py-32 px-4 lg:px-12 pr-4 lg:pr-32"></div>
+    <div className="w-full xl:w-[1500px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
+      {/* particle container */}
+
+      {/* header */}
+      <Header />
+
+      {/* home  */}
+
+      {/* about */}
+
+      {/* skills */}
+
+      {/* project */}
+
+      {/* contact */}
+
+      {/* footer */}
+    </div>
   );
 };
 
